3306541b7fe137e2804a7341edfbf66298a73cd2
h2py <ahdosj@gmail.com>
2025-10-22 04:47:28 +0900

    Fix: fix the error

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+import type { JSX as JSXAstro } from 'astro/jsx-runtime';
+
+declare global {
+  namespace JSX {
+    type Element = JSXAstro.Element;
+    interface ElementChildrenAttribute extends JSXAstro.ElementChildrenAttribute {}
+    interface IntrinsicElements extends JSXAstro.IntrinsicElements {}
+  }
+}
